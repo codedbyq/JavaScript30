7f9ff63a33f85bea48457c933229a41afec4528e
Marquis Bentley <livefreekai@icloud.com>
2021-02-16 21:56:16 -0700

    Grab elements from the document and initialize variables/

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,8 @@
+// get elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = play.querySelector('.progress');
+const progressBar = play.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
